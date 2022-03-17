3d248c4e613751b833e75418fbaad8f7a1fdd971
krishnaveni44 <krishnaveni44@gmail.com>
2022-03-17 22:29:45 +0530

    Heroku-connected

--- src/global.js
+++ src/global.js
@@ -1,2 +1,3 @@
-export const API = "https://6209f23692946600171c55ea.mockapi.io";
+// export const API = "https://6209f23692946600171c55ea.mockapi.io";
 // const API = "https://my-json-server.typicode.com/krishnaveni44/fun-data"
+export const API = "https://krishnavenimovieapp.herokuapp.com";
